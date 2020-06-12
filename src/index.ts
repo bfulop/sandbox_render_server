edebad5148413388ba37725726b55e1811202578
balintfulop <balint.fulop@abtasty.com>
2020-06-12 21:56:11 +0200

    Simple get param working

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,38 @@
-import express from 'express';
-import * as E from 'fp-ts/es6/Either';
-import * as H from 'hyper-ts/es6/index';
-import * as t from 'io-ts/es6/index';
-import {decodeParam} from 'hyper-ts/es6/index';
-import {toRequestHandler} from 'hyper-ts/es6/express';
-import {pipe} from 'fp-ts/es6/pipeable';
+import express from 'express'
+import * as E from 'fp-ts/es6/Either'
+import { pipe } from 'fp-ts/es6/pipeable'
+import * as t from 'io-ts/es6/index'
+import * as H from 'hyper-ts/es6/index'
+import { toRequestHandler } from 'hyper-ts/es6/express'
 
-function badRequest(message: Error) {
+const decodeUser: H.Middleware<H.StatusOpen, H.StatusOpen, string, string> = pipe(
+  H.decodeParam('user_id', t.string.decode),
+  H.mapLeft(() => 'what')
+);
+
+function badRequest(message: string): H.Middleware<H.StatusOpen, H.ResponseEnded, never, void> {
   return pipe(
     H.status(H.Status.BadRequest),
     H.ichain(() => H.closeHeaders()),
-    H.ichain(() => H.send(message.message)),
-  );
+    H.ichain(() => H.send(message))
+  )
 }
 
-// returns a middleware validating `req.param.user_id`
-const decodeUser: H.Middleware<H.StatusOpen, H.StatusOpen, any, any> = pipe(
-  H.decodeParam('user_id', t.string.decode),
-  H.mapLeft(() => new Error('what'))
-);
-
-// returns a middleware validating `req.param.user_id`
-// const decodeUser = pipe(
-//   decodeParam('user_id', t.string.decode),
-//   H.mapLeft(() => new Error('what'))
-// )
-
-const getPage:H.Middleware<H.StatusOpen, H.ResponseEnded, never, any> = pipe(
+const hello = pipe(
   decodeUser,
-  H.ichain(() => pipe(
-    H.status(H.Status.OK),
-    H.ichain(pipe(
-      H.ichain(() => H.closeHeaders()), // tells hyper-ts that we're done with the headers
-      H.ichain(() => H.send('Hello hyper-ts on express!')), // sends the response as text
-    ))
-  ))
-  // H.ichain(() => H.())
-);
+  H.ichain(( name ) =>
+    pipe(
+      H.status<string>(H.Status.OK),
+      H.ichain(() => H.closeHeaders()),
+      H.ichain(() => H.send(`Hello ${name}!`))
+    )
+  ),
+  H.orElse(badRequest)
+)
 
-const hello: H.Middleware<H.StatusOpen, H.ResponseEnded, never, void> = pipe(
-  H.status(H.Status.OK), // writes the response status
-  H.ichain(() => H.closeHeaders()), // tells hyper-ts that we're done with the headers
-  H.ichain(() => H.send('Hello hyper-ts on express!')), // sends the response as text
-);
+const app = express()
 
-express()
-  .get('/', toRequestHandler(hello))
-  .get('/getpage/:userid', toRequestHandler(getPage))
-  .listen(3000, () =>
-    console.log('Express listening on port 3000. Use: GET /'),
-  );
+app
+  .get('/getpage/:user_id', toRequestHandler(hello))
+  // tslint:disable-next-line: no-console
+  .listen(3000, () => console.log('Express listening on port 3000. Use: POST /'))
