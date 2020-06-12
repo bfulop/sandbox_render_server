693b685e22305e39778d942a89a40d32a15129cb
balintfulop <balint.fulop@abtasty.com>
2020-06-12 21:36:23 +0200

    Still not working

--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,42 @@
 import express from 'express';
-import { NonEmptyString } from 'io-ts-types/es6/NonEmptyString'
+import * as E from 'fp-ts/es6/Either';
 import * as H from 'hyper-ts/es6/index';
 import * as t from 'io-ts/es6/index';
+import {decodeParam} from 'hyper-ts/es6/index';
 import {toRequestHandler} from 'hyper-ts/es6/express';
 import {pipe} from 'fp-ts/es6/pipeable';
-import * as TE from 'fp-ts/es6/TaskEither'
-import * as E from 'fp-ts/es6/Either'
 
-const UserId = NonEmptyString
-
-type UserId = NonEmptyString
-
-const UserNotFound: 'UserNotFound' = 'UserNotFound'
-
-const InvalidArguments: 'InvalidArguments' = 'InvalidArguments'
-
-type UserError = typeof InvalidArguments | typeof UserNotFound
-
-// const decodeParamsMiddleware = H.decodeQuery(
-//   t.strict({
-//     pageUrl: t.string,
-//   })
-// ).decode
-
-// function badRequest(message: Error) {
-//  return pipe(
-//    H.status(H.Status.BadRequest),
-//    H.ichain(() => H.closeHeaders()),
-//    H.ichain(() => H.send(message.message))
-//  )
-// }
-
-// function addTwo(a:number, b:number) {
-//   return a + b;
-// }
-
-// addTwo('one', 'two')
-
-// returns a middleware validating `req.param.user_id`
-// const decodeuserid = pipe(
-//   H.decodeParam('userid', t.string.decode),
-//   H.mapLeft(() => new Error('not could decode'))
-// );
-//
-// const doAPIWork = (id:string) =>
-//  H.fromTaskEither(TE.tryCatch(() =>
-//    Promise.resolve(id), E.toError ))
-
-
-function sendPage () {
+function badRequest(message: Error) {
   return pipe(
-    H.status(H.Status.OK),
+    H.status(H.Status.BadRequest),
     H.ichain(() => H.closeHeaders()),
-    H.ichain(() => H.send(`hello `))
-  )
+    H.ichain(() => H.send(message.message)),
+  );
 }
 
-/** Parses the `user_id` param */
-const getUserId = pipe(
-  H.decodeParam('user_id', UserId.decode),
-  H.mapLeft(() => InvalidArguments)
-)
+// returns a middleware validating `req.param.user_id`
+const decodeUser: H.Middleware<H.StatusOpen, H.StatusOpen, any, any> = pipe(
+  H.decodeParam('user_id', t.string.decode),
+  H.mapLeft(() => new Error('what'))
+);
 
-const getPage = sendPage;
-
-// const getPage = pipe(
-//   getUserId,
-//   H.ichain(() => sendPage)
-//   // pipe((user) => pipe(
-//   //   H.ichain(() => sendPage(user))
-//   // ))
+// returns a middleware validating `req.param.user_id`
+// const decodeUser = pipe(
+//   decodeParam('user_id', t.string.decode),
+//   H.mapLeft(() => new Error('what'))
 // )
 
-// const getPage: H.Middleware<H.StatusOpen, H.ResponseEnded, never, void> = pipe(
-//   decodeuserid,
-//   H.ichain((id) => doAPIWork(id)),
-//   H.ichain(userId =>
-//     pipe(
-//       H.status(H.Status.OK), // writes the response status
-//       // H.ichain(() => H.json({myuser: userId}, E.toError))
-//       H.ichain(() => H.closeHeaders()), // tells hyper-ts that we're done with the headers
-//       H.ichain(() => H.send(`Hello hyper-ts on express! userid: ${userId}`)), // sends the response as text
-//     ),
-//   ),
-//   H.orElse(badRequest)
-// );
+const getPage:H.Middleware<H.StatusOpen, H.ResponseEnded, never, any> = pipe(
+  decodeUser,
+  H.ichain(() => pipe(
+    H.status(H.Status.OK),
+    H.ichain(pipe(
+      H.ichain(() => H.closeHeaders()), // tells hyper-ts that we're done with the headers
+      H.ichain(() => H.send('Hello hyper-ts on express!')), // sends the response as text
+    ))
+  ))
+  // H.ichain(() => H.())
+);
 
 const hello: H.Middleware<H.StatusOpen, H.ResponseEnded, never, void> = pipe(
   H.status(H.Status.OK), // writes the response status
