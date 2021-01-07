dc5a5f1b38f7c07ee12979631ff6d1e7d151f2e6
balintfulop <balint.fulop@abtasty.com>
2021-01-07 18:56:18 +0100

    convert a stream of anything to a stream of handled events

--- src/pipetests.ts
+++ src/pipetests.ts
@@ -1,75 +1,123 @@
-import { of } from 'rxjs';
-import { Decoder, success, failure, literal, string, sum, type, number, TypeOf, union } from 'io-ts/es6/Decoder'
-import { isRight, parseJSON, toError, chain } from 'fp-ts/es6/Either'
+import { of, Observable } from 'rxjs';
+import {
+  Decoder,
+  success,
+  failure,
+  literal,
+  string,
+  sum,
+  type,
+  number,
+  TypeOf,
+  union,
+} from 'io-ts/es6/Decoder';
+import {
+  isRight,
+  parseJSON,
+  toError,
+  chain as chainE,
+  Json,
+  left,
+  right,
+} from 'fp-ts/es6/Either';
 import { fromEither } from 'fp-ts/es6/Option';
-import { pipe, flow } from 'fp-ts/es6/function';
-import { filterMap } from 'fp-ts-rxjs/es6/Observable';
+import type { Either } from 'fp-ts/es6/Either';
+import { pipe } from 'fp-ts/es6/function';
+import { filterMap, map as mapO } from 'fp-ts-rxjs/es6/Observable';
+import type { ObservableEither } from 'fp-ts-rxjs/es6/ObservableEither';
+import { map as mapOE } from 'fp-ts-rxjs/es6/ObservableEither';
 
 console.clear();
-const mystream = of('a', 'b', 'a', 'b', 'a', JSON.stringify({type: 'click', x: 1, y: 2, path: 'what'}));
+const mystream = of(
+  'a',
+  'b',
+  3,
+  'a',
+  5,
+  'b',
+  'a',
+  JSON.stringify({ type: 'click', x: 1, y: 2, path: 'what' }),
+  JSON.stringify({ umbrella: 'potatoes' })
+);
 
-export const UserEvents: Decoder<
-  unknown,
-  | {
-    type: 'mousemove',
-    x: number
-    y: number
-  }
-  | {
-    type: 'click',
-    x: number
-    y: number
-    path: string
-  }
-> = sum('type')({
+type mousemove = {
+  type: 'mousemove';
+  x: number;
+  y: number;
+};
+type click = {
+  type: 'click';
+  x: number;
+  y: number;
+  path: string;
+};
+export const UserEvents: Decoder<unknown, mousemove | click> = sum('type')({
   mousemove: type({ type: literal('mousemove'), x: number, y: number }),
-  click: type({ type: literal('click'), x: number, y: number, path: string })
-})
+  click: type({ type: literal('click'), x: number, y: number, path: string }),
+});
 
-export type UserEvents = TypeOf<typeof UserEvents>
+export type UserEvents = TypeOf<typeof UserEvents>;
+type HandledEvents = mousemove | click;
 
 export const SystemEvents: Decoder<
   unknown,
   | {
-    type: 'DOMpatched'
-  }
+      type: 'DOMpatched';
+    }
   | {
-    type: 'listeningToDOMDiffs'
-  }
+      type: 'listeningToDOMDiffs';
+    }
 > = sum('type')({
-  DOMpatched: type({ type: literal('DOMpatched')}),
-  listeningToDOMDiffs: type({ type: literal('listeningToDOMDiffs')})
-})
+  DOMpatched: type({ type: literal('DOMpatched') }),
+  listeningToDOMDiffs: type({ type: literal('listeningToDOMDiffs') }),
+});
 
-export const KnownEvent = union(UserEvents, SystemEvents)
-type KnownEvent = TypeOf<typeof KnownEvent>
+export const KnownEvent = union(UserEvents, SystemEvents);
+export type KnownEvent = TypeOf<typeof KnownEvent>;
 
 const mystring: Decoder<unknown, string> = {
-  decode: (u) => (typeof u === 'string' ? success(u) : failure(u, 'string'))
+  decode: (u) => (typeof u === 'string' ? success(u) : failure(u, 'string')),
+};
+
+console.log(
+  isRight(
+    KnownEvent.decode({
+      type: 'user',
+      event: { type: 'click', x: 1, y: 2, path: 'what' },
+    })
+  )
+); // => true
+console.log(
+  isRight(UserEvents.decode({ type: 'click', x: 1, y: 2, path: 'what' }))
+); // => true
+console.log(isRight(mystring.decode(null))); // => false
+
+const doParseJson = (a: string): Either<Error, Json> => parseJSON(a, toError);
+const doDecode = (a: Json): Either<Error, HandledEvents> =>
+  UserEvents.decode(a);
+
+function isString(text: unknown): Either<Error, string> {
+  return typeof text === 'string' ? right(text) : left(Error('not a text'));
 }
 
-console.log(isRight(KnownEvent.decode({type: 'user', event: {type: 'click', x: 1, y: 2, path: 'what'}}))) // => true
-console.log(isRight(UserEvents.decode({type: 'click', x: 1, y: 2, path: 'what'}))) // => true
-console.log(isRight(mystring.decode(null))) // => false
+const jsonToUserEvent = (somejson: string): Either<Error, HandledEvents> =>
+  pipe(somejson, doParseJson, chainE(doDecode));
+const toUserEvents$ = (
+  stream: Observable<unknown>
+): ObservableEither<Error, HandledEvents> =>
+  pipe(stream, mapO(isString), mapOE(jsonToUserEvent), filterMap(fromEither));
 
-const doParseJson = (a: string) => parseJSON(a, toError)
+const decodedEvents$ = (
+  stream: ObservableEither<Error, HandledEvents>
+): Observable<HandledEvents> => pipe(stream, filterMap(fromEither));
 
-export const KnownEvents$ = pipe(
-  filterMap(flow(
-    doParseJson,
-    chain(KnownEvent.decode),
-    fromEither
-  ))
-)
+export const userEvents$ = (
+  stream: Observable<unknown>
+): Observable<HandledEvents> => pipe(stream, toUserEvents$, decodedEvents$);
 
-
-console.log('streampipe')
-KnownEvents$(mystream).subscribe(a => {
-  console.log(a)
-})
-// filteredStreamB.subscribe(a => {
-//   console.log(a)
-// })
-// console.log(streampipe)
-
-
+console.clear();
+console.log('  ---------------  streampipe  ---------------  ');
+pipe(mystream, toUserEvents$, decodedEvents$).subscribe((e) => {
+  console.log('****************************************');
+  console.log(e);
+});
--- vanilla/pipetests.js
+++ vanilla/pipetests.js
@@ -1,42 +1,42 @@
 import { of } from 'rxjs';
-import { success, failure, literal, string, sum, type, number, union } from 'io-ts/es6/Decoder';
-import { isRight, parseJSON, toError } from 'fp-ts/es6/Either';
+import { success, failure, literal, string, sum, type, number, union, } from 'io-ts/es6/Decoder';
+import { isRight, parseJSON, toError, chain as chainE, left, right, } from 'fp-ts/es6/Either';
 import { fromEither } from 'fp-ts/es6/Option';
 import { pipe } from 'fp-ts/es6/function';
-import { filterMap, map } from 'fp-ts-rxjs/es6/Observable';
+import { filterMap, map as mapO } from 'fp-ts-rxjs/es6/Observable';
 import { map as mapOE } from 'fp-ts-rxjs/es6/ObservableEither';
-// import { map } from 'fp-ts-rxjs/es6/ObservableEither'
 console.clear();
-const mystream = of('a', 'b', 3, 'a', 5, 'b', 'a', JSON.stringify({ type: 'click', x: 1, y: 2, path: 'what' }));
+const mystream = of('a', 'b', 3, 'a', 5, 'b', 'a', JSON.stringify({ type: 'click', x: 1, y: 2, path: 'what' }), JSON.stringify({ umbrella: 'potatoes' }));
 export const UserEvents = sum('type')({
     mousemove: type({ type: literal('mousemove'), x: number, y: number }),
-    click: type({ type: literal('click'), x: number, y: number, path: string })
+    click: type({ type: literal('click'), x: number, y: number, path: string }),
 });
 export const SystemEvents = sum('type')({
     DOMpatched: type({ type: literal('DOMpatched') }),
-    listeningToDOMDiffs: type({ type: literal('listeningToDOMDiffs') })
+    listeningToDOMDiffs: type({ type: literal('listeningToDOMDiffs') }),
 });
 export const KnownEvent = union(UserEvents, SystemEvents);
 const mystring = {
-    decode: (u) => (typeof u === 'string' ? success(u) : failure(u, 'string'))
+    decode: (u) => (typeof u === 'string' ? success(u) : failure(u, 'string')),
 };
-console.log(isRight(KnownEvent.decode({ type: 'user', event: { type: 'click', x: 1, y: 2, path: 'what' } }))); // => true
+console.log(isRight(KnownEvent.decode({
+    type: 'user',
+    event: { type: 'click', x: 1, y: 2, path: 'what' },
+}))); // => true
 console.log(isRight(UserEvents.decode({ type: 'click', x: 1, y: 2, path: 'what' }))); // => true
 console.log(isRight(mystring.decode(null))); // => false
 const doParseJson = (a) => parseJSON(a, toError);
-const myresult = number.decode(123);
-const resulter = (a) => number.decode(a);
-const KnownEvents$ = (astream) => pipe(astream, map(resulter));
-const incrementStream = (somestream) => pipe(somestream, mapOE(a => a * 2));
-const rightStream = (somestream) => pipe(somestream, filterMap(fromEither));
-console.log('streampipe');
-pipe(mystream, KnownEvents$, incrementStream, rightStream).subscribe(a => {
-    console.log(a);
+const doDecode = (a) => UserEvents.decode(a);
+function isString(text) {
+    return typeof text === 'string' ? right(text) : left(Error('not a text'));
+}
+const jsonToUserEvent = (somejson) => pipe(somejson, doParseJson, chainE(doDecode));
+const toUserEvents$ = (stream) => pipe(stream, mapO(isString), mapOE(jsonToUserEvent), filterMap(fromEither));
+const decodedEvents$ = (stream) => pipe(stream, filterMap(fromEither));
+export const userEvents$ = (stream) => pipe(stream, toUserEvents$, decodedEvents$);
+console.clear();
+console.log('  ---------------  streampipe  ---------------  ');
+pipe(mystream, toUserEvents$, decodedEvents$).subscribe((e) => {
+    console.log('****************************************');
+    console.log(e);
 });
-// KnownEvents$(mystream).subscribe(a => {
-//   console.log(a)
-// })
-// filteredStreamB.subscribe(a => {
-//   console.log(a)
-// })
-// console.log(streampipe)
