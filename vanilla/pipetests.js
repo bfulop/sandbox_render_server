b71eaabae43396be53374f9acc71fe1f5a673c27
balintfulop <balint.fulop@abtasty.com>
2021-01-07 16:35:15 +0100

    Decode a stream first test

--- vanilla/pipetests.js
+++ vanilla/pipetests.js
@@ -1,11 +1,13 @@
 import { of } from 'rxjs';
 import { success, failure, literal, string, sum, type, number, union } from 'io-ts/es6/Decoder';
-import { isRight, parseJSON, toError, chain } from 'fp-ts/es6/Either';
+import { isRight, parseJSON, toError } from 'fp-ts/es6/Either';
 import { fromEither } from 'fp-ts/es6/Option';
-import { pipe, flow } from 'fp-ts/es6/function';
-import { filterMap } from 'fp-ts-rxjs/es6/Observable';
+import { pipe } from 'fp-ts/es6/function';
+import { filterMap, map } from 'fp-ts-rxjs/es6/Observable';
+import { map as mapOE } from 'fp-ts-rxjs/es6/ObservableEither';
+// import { map } from 'fp-ts-rxjs/es6/ObservableEither'
 console.clear();
-const mystream = of('a', 'b', 'a', 'b', 'a', JSON.stringify({ type: 'click', x: 1, y: 2, path: 'what' }));
+const mystream = of('a', 'b', 3, 'a', 5, 'b', 'a', JSON.stringify({ type: 'click', x: 1, y: 2, path: 'what' }));
 export const UserEvents = sum('type')({
     mousemove: type({ type: literal('mousemove'), x: number, y: number }),
     click: type({ type: literal('click'), x: number, y: number, path: string })
@@ -22,11 +24,18 @@ console.log(isRight(KnownEvent.decode({ type: 'user', event: { type: 'click', x:
 console.log(isRight(UserEvents.decode({ type: 'click', x: 1, y: 2, path: 'what' }))); // => true
 console.log(isRight(mystring.decode(null))); // => false
 const doParseJson = (a) => parseJSON(a, toError);
-export const KnownEvents$ = pipe(filterMap(flow(doParseJson, chain(KnownEvent.decode), fromEither)));
+const myresult = number.decode(123);
+const resulter = (a) => number.decode(a);
+const KnownEvents$ = (astream) => pipe(astream, map(resulter));
+const incrementStream = (somestream) => pipe(somestream, mapOE(a => a * 2));
+const rightStream = (somestream) => pipe(somestream, filterMap(fromEither));
 console.log('streampipe');
-KnownEvents$(mystream).subscribe(a => {
+pipe(mystream, KnownEvents$, incrementStream, rightStream).subscribe(a => {
     console.log(a);
 });
+// KnownEvents$(mystream).subscribe(a => {
+//   console.log(a)
+// })
 // filteredStreamB.subscribe(a => {
 //   console.log(a)
 // })
